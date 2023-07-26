35b8ae0cbda696b431cd01bc347d499b39c45eed
Ben Huang <benhuang011@gmail.com>
2023-07-25 21:01:05 -0700

    create function to update profile name and email to firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,54 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { doc, updateDoc } from 'firebase/firestore'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
 
 function Profile() {
 
   const auth = getAuth()
+  const navigate = useNavigate()
+  const [changeDetail, setChangeDetail] = useState(false)
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   })
 
   const {name, email} = formData
-  const navigate = useNavigate()
-  
-  function onLogout(){
-    auth.signOut()
+
+  async function onLogout(){
+    await auth.signOut()
     navigate('/')
   }
 
+  function onChange(e){
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }))
+  }
+  async function onSubmit(){
+    try{
+      if(auth.currentUser.displayName !== name){
+        // update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+      }
 
+      // update name in the firestore database
+      const docRef = doc(db, "users", auth.currentUser.uid)
+      await updateDoc(docRef, {
+        name: name,
+      })
+
+      toast.success("Profile is Updated.")
+    }catch(error){
+      toast.error("Could not update the profile Detail")
+    }
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -27,14 +57,37 @@ function Profile() {
 
         <div className="w-full md:w-[50%] mt-6 px-3">
           <form>
-          <input type="text" id="name" value={name} disabled className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
-          <input type="text" id="email" value={email} disabled className="w-full  mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out" />
+          <input 
+            type="text" 
+            id="name" 
+            value={name} 
+            disabled={!changeDetail}
+            onChange={onChange}
+            className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
+
+          <input 
+            type="text" 
+            id="email" 
+            value={email} 
+            disabled={!changeDetail}
+            className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
 
           <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
             <p className="flex items-center ">Do you want to change your name? 
-              <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"> Edit</span>
+              <span 
+                onClick={() => {
+                  changeDetail && onSubmit()
+                  setChangeDetail(prevState => !prevState)
+                }} 
+                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  {changeDetail ? "Apply Change" : "Edit"}
+              </span>
             </p>
-            <p onClick={onLogout}className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 ml-1 cursor-pointer">Sign Out</p>
+            <p 
+              onClick={onLogout}
+              className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                Sign Out
+            </p>
           </div>
           </form>
         </div>
