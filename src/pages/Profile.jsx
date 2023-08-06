18336532b2df9da5398b1224e1555d24725d343d
Ben Huang <benhuang011@gmail.com>
2023-08-06 11:31:38 -0700

    restore back

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,11 +4,9 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, NavLink } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { db } from '../firebase'
+import { FcHome } from 'react-icons/fc'
 import ListingItem from '../components/ListingItem'
-import { Tab } from '@headlessui/react'
-import { GiFamilyHouse } from "react-icons/gi"
-import { MdManageAccounts } from "react-icons/md"
-import { AiOutlinePlus } from "react-icons/ai"
+
 function Profile() {
 
   const auth = getAuth()
@@ -35,7 +33,6 @@ function Profile() {
       [e.target.id] : e.target.value
     }))
   }
-  
   async function onSubmit(){
     try{
       if(auth.currentUser.displayName !== name){
@@ -104,114 +101,74 @@ function Profile() {
 
   return (
     <>
-    <section className="profile">
-      <div className="profile-Wrapper lg:flex max-w-[1440px] mx-auto p-8">
-      <Tab.Group>
-          <Tab.List className="profile-list flex basis-1/6 lg:flex-col text-xl mt-6">
-              <Tab className="text-left flex items-center gap-2 mb-6 px-8 focus:ring-0">
-                <GiFamilyHouse />
-                <span className="hidden md:block">Listings</span>
-              </Tab>
-         
-              <Tab className="text-left flex items-center gap-2 mb-6 px-8 focus:ring-0">
-                <MdManageAccounts />
-                <span className="hidden md:block">Account</span>
-              </Tab>
-          </Tab.List>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+    
 
-          <Tab.Panels className="basis-4/6 ml-auto mt-6 ">
-            <Tab.Panel>
-            <div className="max-w-6xl px-3 mx-auto">
-              {!loading && listings.length > 0 && (
-                <>
-                <div className="border-b pb-5 flex justify-between">
-                  <h2 className="text-4xl font-semibold flex items-center gap-2">
-                    <GiFamilyHouse />
-                    My Listings
-                  </h2>
-                 
-                  
-                </div>
-                  <ul className="grid md:grid-cols-2 mt-6 mb-6 gap-5">
-                    <li className=" border-2 border-dashed text-gray-400 flex justify-center items-center hover:border-black hover:text-black transition duration-200 ease-in-out">
-                      <button type="submit" className="w-full h-[100px] lg:h-full uppercase px-7 py-3 text-sm font-medium">
-                        
-                        <NavLink to="/create-listing" className="w-full h-full flex justify-center items-center gap-2">
-                          <AiOutlinePlus />
-                          Sell or rent your home
-                        </NavLink> 
-                      </button>
-                    </li>
-                    {listings.map((listing) => (
-                      <ListingItem 
-                        key={listing.id} 
-                        id={listing.id} 
-                        listing={listing.data}
-                        onDelete={()=> onDelete(listing.id)} //trigger a function get the listing id, and know which one to delete
-                        onEdit={()=> onEdit(listing.id)} //trigger a function get the listing id, and know which one to edit
-                      />
-                    ))}
-                  </ul>
-                </>
-              )}
-            </div>
-          </Tab.Panel>
-            <Tab.Panel>
-              <div className="max-w-6xl px-3 mx-auto">
-                <h2 className="text-4xl font-semibold flex items-center gap-2 border-b pb-5">
-                  <MdManageAccounts />
-                  Account
-                </h2>
-                <h3 className="text-2xl py-5">
-                  Account Profile
-                </h3>
-                <form className="w-full md:w-[50%]">
-                  <div className='border border-gray-300 rounded py-2 px-5 mb-6 border-s-4 border-s-[#D8C095]'>
-                    <label>Name</label>
-                    <input 
-                      type="text" 
-                      id="name" 
-                      value={name} 
-                      disabled={!changeDetail}
-                      onChange={onChange}
-                      className="w-full text-xl p-0 text-gray-700 bg-white border-none focus:ring-0"
-                    />
-                  </div>
-                  
-                  <div className='border border-gray-300 rounded py-2 px-5 mb-6 border-s-4 border-s-[#D8C095]'>
-                    <label>Email</label>
-                    <input 
-                      type="text" 
-                      id="email" 
-                      value={email} 
-                      disabled={!changeDetail}
-                      className="w-full text-xl p-0 text-gray-700 bg-white border-none focus:ring-0 "
-                    />
-                  </div>
-                  
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+          <input 
+            type="text" 
+            id="name" 
+            value={name} 
+            disabled={!changeDetail}
+            onChange={onChange}
+            className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
 
-                  <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6 text-center">
-                      <span
-                        onClick={() => {
-                          changeDetail && onSubmit()
-                          setChangeDetail(prevState => !prevState)
-                        }} 
-                        className="w-full bg-[#BF974F] text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md transition duration-200 ease-in-out hover:shadow-lg cursor-pointer">
-                          {changeDetail ? "Apply Change" : "Edit"}
-                      </span>
-                    {/* <p 
-                      onClick={onLogout}
-                      className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
-                        Sign Out
-                    </p> */}
-                  </div>
-                </form>
-              </div>
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+          <input 
+            type="text" 
+            id="email" 
+            value={email} 
+            disabled={!changeDetail}
+            className={`w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}/>
+
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+            <p className="flex items-center ">Do you want to change your name? 
+              <span 
+                onClick={() => {
+                  changeDetail && onSubmit()
+                  setChangeDetail(prevState => !prevState)
+                }} 
+                className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  {changeDetail ? "Apply Change" : "Edit"}
+              </span>
+            </p>
+            <p 
+              onClick={onLogout}
+              className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                Sign Out
+            </p>
+          </div>
+
+          <div>
+            <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800">
+              <NavLink to="/create-listing" className="flex justify-center items-center">
+                <FcHome className="me-2 text-3xl bg-red-200 rounded-full p-1 border-2"/>
+                Sell or rent your home
+              </NavLink> 
+            </button>
+          </div>
+          </form>
+        </div>
+      </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
+            {listings.map((listing) => (
+              <ListingItem 
+                key={listing.id} 
+                id={listing.id} 
+                listing={listing.data}
+                onDelete={()=> onDelete(listing.id)} //trigger a function get the listing id, and know which one to delete
+                onEdit={()=> onEdit(listing.id)} //trigger a function get the listing id, and know which one to edit
+              />
+            ))}
+            </ul>
+          </>
+        )}
       </div>
-    </section>
     </>
   )
 }
