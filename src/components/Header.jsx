d657690fef8a1f99e29d721a8b8590ce34fc6e5c
Ben Huang <benhuang011@gmail.com>
2023-08-07 19:38:13 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,11 +31,11 @@ function Header() {
  
     return (
         <div className="bg-white border-b shadow-sm sticky top-0 z-40">
-            <header className='flex justify-between items-center px-3 max-w-[1440px] mx-auto'>
+            <header className='flex justify-between items-center px-8 max-w-[1440px] mx-auto'>
                 <div>
                     <NavLink to='/'><img src="https://www.realtor.ca/images/logo.svg" alt="logo" className='cursor-pointer h-9' /></NavLink>
                 </div>
-                <div>
+                <div className="flex items-center">
                     <ul className='flex gap-5'>
                         <li className="cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent">
                             <NavLink to='/' className={({ isActive }) => isActive ? "text-black border-b-[3px] py-3 border-b-red-500" : ""}>Home</NavLink>
@@ -44,19 +44,23 @@ function Header() {
                             <NavLink to='/offers' className={({ isActive }) => isActive ? "text-black border-b-[3px] py-3 border-b-red-500" : ""}>Offer</NavLink>
                         </li>
                         <li className="cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ">
-                            <NavLink to={checkSignIn ? "/profile" : "/sign-in"} className={({ isActive }) => isActive ? "text-black border-b-[3px] py-3 border-b-red-500" : ""}>{checkSignIn ? `Hello! ${firstName}` : "Sign In" }</NavLink>
+                            <NavLink to={checkSignIn ? "/profile" : "/sign-in"} className={({ isActive }) => isActive ? "text-black border-b-[3px] py-3 border-b-red-500" : ""}>
+                                {checkSignIn ? `Hello! ${firstName}` : "Sign In" }
+                            </NavLink>
                         </li>
                         <li className="cursor-pointer my-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent">
-                            {checkSignIn &&
-                                <p 
-                                    onClick={onLogout}
-                                    className={({ isActive }) => isActive ? "text-black border-b-[3px] py-3 border-b-red-500" : ""}>
-                                    Sign Out
-                                </p>
-                            }
                         </li>
                     </ul>
+                    <div>
+                        {checkSignIn &&
+                            <h3 
+                                onClick={onLogout}>
+                                Sign Out
+                            </h3>
+                        }
+                    </div>
                 </div>
+                
             </header>
         </div>
     )
